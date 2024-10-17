5af9c297dbace2400e505bf80017112e6611dbe9
Athena20230322 <autoicash2023@gmail.com>
2024-10-17 10:59:27 +0800

    updateios_icash2.0

---
+++ icash-icash2.0.air/icash-icash2.0.py
@@ -0,0 +1,29 @@
+# -*- encoding=utf8 -*-
+__author__ = "lun"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+swipe(Template(r"tpl1729045866034.png", record_pos=(-0.315, -0.812), resolution=(1284, 2778)), vector=[0.1007, 0.012])
+touch(Template(r"tpl1729045870038.png", record_pos=(0.383, -0.634), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045874051.png", record_pos=(-0.354, -0.78), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045880045.png", record_pos=(-0.416, -0.923), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045884784.png", record_pos=(-0.111, -0.78), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045888626.png", record_pos=(-0.422, -0.92), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045891249.png", record_pos=(0.127, -0.764), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045895246.png", record_pos=(-0.41, -0.92), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045898103.png", record_pos=(0.341, -0.754), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045903434.png", record_pos=(-0.416, -0.92), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045905916.png", record_pos=(-0.354, -0.585), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045910598.png", record_pos=(-0.413, -0.917), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045913193.png", record_pos=(-0.1, -0.585), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045917930.png", record_pos=(-0.426, -0.917), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045920464.png", record_pos=(0.133, -0.601), resolution=(1284, 2778)))
+swipe(Template(r"tpl1729045924728.png", record_pos=(0.419, 0.208), resolution=(1284, 2778)), vector=[0.0032, 0.0])
+touch(Template(r"tpl1729045928360.png", record_pos=(-0.432, -0.914), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045931628.png", record_pos=(0.419, -0.312), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045935161.png", record_pos=(-0.422, -0.929), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045937554.png", record_pos=(0.091, -0.312), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045939693.png", record_pos=(0.091, -0.312), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045943133.png", record_pos=(-0.419, -0.914), resolution=(1284, 2778)))
+touch(Template(r"tpl1729045965762.png", record_pos=(-0.413, -0.783), resolution=(1284, 2778)))
